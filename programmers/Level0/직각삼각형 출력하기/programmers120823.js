bf3ea28058efe21240be4ea1948dee7ed974998a
ygkim97 <dnjdn21@naver.com>
2023-04-11 13:26:27 +0900

    programmers: 직각삼각형 출력하기

---
+++ programmers/Level0/직각삼각형 출력하기/programmers120823.js
@@ -0,0 +1,25 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  const n = Number(input[0]);
+
+  const result = new Array(n)
+    .fill()
+    .map((_, i) => "*".repeat(i + 1))
+    .join("\n");
+  console.log(result);
+
+  /*
+  for (let i = 1; i <= n; i++) {
+    console.log("*".repeat(i));
+  }
+  */
+});
